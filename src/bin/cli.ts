41c42ced2095f5ab03c85b24230a5356fe0e01d6
William Luke <william@ordino.ai>
2020-12-31 09:42:43 +0300

    feat: create dev branch

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -1,24 +1,27 @@
 import chalk from "chalk";
 import execa from "execa";
-import inquirer from "inquirer";
-import meow from "meow";
-import path from "path";
-import { ExampleRepo, EXAMPLE_REPOS } from "../examples";
-import { fetchContent, GithubDownloader } from "../lib/github-download";
-import { client, Settings } from '../lib/prisma'
 import fs from "fs-jetpack";
 import fuzzy from "fuzzy";
+import indentString from "indent-string";
+import inquirer from "inquirer";
+import meow from "meow";
 import os from "os";
-import { updater } from "../lib/update";
+import path from "path";
+import type { PackageJson } from "types-package-json";
+import { ExampleRepo, EXAMPLE_REPOS } from "../examples";
 import { getCurrentUser } from "../lib/getUser";
+import { fetchContent, GithubDownloader } from "../lib/github-download";
+import logger, { spinner } from "../lib/logger";
+import { MD_REGEX } from "../lib/markdown";
+import { client, Settings } from "../lib/prisma";
 import { sync } from "../lib/sync";
-import logger from "../lib/logger";
+import { updater } from "../lib/update";
 
 const homedir = os.homedir();
-const configDir = fs.dir(path.join(homedir, '.config', 'fster'))
-const configDB = configDir.path('config.db')
-if(!fs.exists(configDB)){
-  fs.copy(path.join(__dirname, '..', '..', 'config.db'), configDB)
+const configDir = fs.dir(path.join(homedir, ".config", "fster"));
+const configDB = configDir.path("config.db");
+if (!fs.exists(configDB)) {
+  fs.copy(path.join(__dirname, "..", "..", "config.db"), configDB);
 }
 
 inquirer.registerPrompt(
@@ -26,7 +29,7 @@ inquirer.registerPrompt(
   require("inquirer-autocomplete-prompt")
 );
 function printSettings(settings: Settings | null, updated?: boolean) {
-  logger.log(chalk.bold(`\n  ${updated ? 'Updated ': ''}Settings`));
+  logger.log(chalk.bold(`\n  ${updated ? "Updated " : ""}Settings`));
   settings &&
     Object.keys(settings).forEach((key) => {
       if (!["id", "userId"].includes(key)) {
@@ -48,12 +51,17 @@ function search<T>(search: T[], getter: (value: T) => string, input: string) {
   });
 }
 
-const pkg = fs.read(path.join(__dirname,"..","..", "package.json"), "json")
+const pkg = fs.read(
+  path.join(__dirname, "..", "..", "package.json"),
+  "json"
+) as Partial<PackageJson>;
 async function run() {
-  const updated = await updater({name: pkg.name, version: pkg.version})
-  if(updated){
-    logger.success('You may now rerun the last command');
-    process.exit();
+  if (pkg.version && pkg.name && !pkg.name.includes('dev')) {
+    const updated = await updater({ name: pkg.name, version: pkg.version });
+    if (updated) {
+      logger.success("You may now rerun the last command");
+      process.exit();
+    }
   }
   const currentUser = await getCurrentUser();
 
@@ -81,32 +89,35 @@ async function run() {
         version: {
           alias: "v",
           type: "boolean",
-        }
+        },
       },
     }
   );
-  if(cli.input[0] === 'template'){
+
+  if (cli.input[0] === "template") {
     const argOutputDir = cli.input[1];
     const { who } = await inquirer.prompt([
       {
         type: "list",
         name: "who",
         required: true,
-        default: 'prisma',
+        default: "prisma",
         message: "Who's examples are you looking for",
         pageSize: Object.keys(EXAMPLE_REPOS).length,
         choices: Object.keys(EXAMPLE_REPOS),
       },
     ]);
-    const examples = EXAMPLE_REPOS[who as keyof typeof EXAMPLE_REPOS]
+    const examples = EXAMPLE_REPOS[who as keyof typeof EXAMPLE_REPOS];
     const found = await loop(examples);
-  
+
     if (found && found.isExample && found.path) {
       const dir = path.basename(found.path);
       const outputDir = path.join(
         process.cwd(),
         argOutputDir ? argOutputDir : dir
       );
+      const spin = spinner("Downloading");
+      spin.start();
       const gh = new GithubDownloader({
         user: examples.user,
         repo: examples.repo,
@@ -115,44 +126,96 @@ async function run() {
         outputDir,
       });
       gh.on("end", async () => {
-        logger.success(`Downloaded`);
+        spin.stop();
         // Install Deps
-        const pkgMnger = currentUser?.settings?.packageManager ?? 'npm'
-        const pkgMngerArgs = pkgMnger === 'npm' ? ['install'] : []
-        const installProcess = await execa(pkgMnger, pkgMngerArgs, {
-          cwd: outputDir,
-          stdio: "inherit",
-        });
-        logger.success(`Installing`);
+        const pkgMnger = currentUser?.settings?.packageManager ?? "npm";
+        const pkgMngerArgs = pkgMnger === "npm" ? ["install"] : [];
+        await logger.run(
+          "Installing",
+          execa(pkgMnger, pkgMngerArgs, {
+            cwd: outputDir,
+            stdio: "ignore",
+          })
+        );
 
         // Clean Git
-        const rmGitProcess = await execa(`rm`, ["-rf", "./.git"], {
-          cwd: outputDir,
-          stdio: "inherit",
-        });
+        await logger.run(
+          "Cleaning git",
+          execa(`rm`, ["-rf", "./.git"], {
+            cwd: outputDir,
+            stdio: "ignore",
+          })
+        );
         // New git
-        const newGitProcess = await execa(`git`, ["init"], {
-          cwd: outputDir,
-          stdio: "inherit",
-        });
+        await logger.run(
+          "Initializing git",
+          execa(`git`, ["init"], {
+            cwd: outputDir,
+            stdio: "ignore",
+          })
+        );
         // Add git
-        const addGitProcess = await execa(`git`, ["add", '.'], {
-          cwd: outputDir,
-          stdio: "inherit",
-        });
+        await logger.run(
+          "Adding files to git",
+          execa(`git`, ["add", "."], {
+            cwd: outputDir,
+            stdio: "ignore",
+          })
+        );
         // Commit
-        const commitGitProcess = await execa(`git`, ["commit", "-m", `"fster(template): https://github.com/${examples.user}/${examples.repo}/tree/${examples.defaultRef}${found.path}"`], {
-          cwd: outputDir,
-          stdio: "inherit",
+        const commitMessage = `"fster(template): https://github.com/${examples.user}/${examples.repo}/tree/${examples.defaultRef}${found.path}"`;
+        await logger.run(
+          `Added commit - ${commitMessage}`,
+          execa(`git`, ["commit", "-m", commitMessage], {
+            cwd: outputDir,
+            stdio: "ignore",
+          })
+        );
+        const mdFiles = fs.find({
+          files: true,
+          directories: false,
+          ignoreCase: true,
+          matching: "*.md",
         });
-        logger.success(`Complete`);
-        // Open In Editor
-        const editorProcess = execa(currentUser?.settings?.editor ?? 'code', ['.'], {
-          cwd: outputDir,
-          detached: true,
-          stdio: 'ignore'
-        });
-        editorProcess.unref()
+        const file = mdFiles.find((f) => MD_REGEX.exec(f));
+        const parsed = fs.read(path.join(outputDir, "package.json"), "json");
+
+        if (parsed && parsed.scripts) {
+          const longest = Object.keys(parsed.scripts).reduce(
+            (longest, name) => (name.length > longest ? name.length : longest),
+            0
+          );
+          const display = Object.keys(parsed.scripts).reduce((acc, name) => {
+            const space = longest - name.length;
+            acc += `${" ".repeat(space / 2)}${chalk.inverse(
+              ` ${name} `
+            )}${" ".repeat(space / 2)}  ${chalk.dim(parsed.scripts[name])}\n`;
+            return acc;
+          }, "\n");
+          logger.log(chalk.bold.underline("\nScripts"));
+          logger.log(indentString(display, 2));
+        }
+        if (
+          file &&
+          (await logger.confirm(
+            `Would you like open this project using ${chalk.inverse(
+              ` ${currentUser.settings?.editor} `
+            )}`
+          ))
+        ) {
+          // printMD({ path: file });
+          // Open In Editor
+          const editorProcess = execa(
+            currentUser?.settings?.editor ?? "code",
+            ["."],
+            {
+              cwd: outputDir,
+              detached: true,
+              stdio: "ignore",
+            }
+          );
+          editorProcess.unref();
+        }
       });
       gh.download();
     }
@@ -160,8 +223,8 @@ async function run() {
   if (cli.input[0] === "local") {
     let projects = await client.project.findMany();
     sync(currentUser).then((prjs) => {
-      projects=prjs
-    })
+      projects = prjs;
+    });
     // const argOutputDir = cli.input[0];
     const { repo } = await inquirer.prompt([
       {
@@ -179,9 +242,7 @@ async function run() {
       if (fs.exists(project.path)) {
         execa(currentUser.settings?.editor ?? "code", [project.path]);
       } else {
-        logger.error(
-          `Project path no longer exists, please rerun`
-        );
+        logger.error(`Project path no longer exists, please rerun`);
         await client.project.delete({
           where: {
             url: project.url,
